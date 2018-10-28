3f5b4711d70f76261286f07980e657df0f66fea5
林子胜 <zishenglin1995@gmail.com>
2018-10-28 13:23:57 -0700

    final

---
+++ exercise1/src/main/java/BSDSexercise/App.java
@@ -0,0 +1,13 @@
+package BSDSexercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
